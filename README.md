7417c95a4bc5e32c03934aa3ce722f645954a32d
rj9935 <rj9935@telus.net>
2015-01-14 14:03:34 -0800

    1st Commit run_analysis.R

---
+++ README.md
@@ -0,0 +1,88 @@
+---
+title: "Merge UCI HAR data sets, tidy and summarize"
+author: "Ray Jones"
+date: "January 14, 2015"
+output: html_document
+---
+
+### Description
+
+Run_analysis.R is a script produced to satisfy the Project assignment of the Getting & Cleaning Data course from the Data Science Specialization. It takes various inputs from the UCI HAR data set and merges them to produce a wide format tidy set of the means of various measurements selected from the inputs. It then outputs these mean values with appropriate and descriptive variable names to a file called meandatawide.txt  
+ 
+### Arguments
+
+None.  
+
+### Details
+
+Step 1 creates the merged data set:  
+
+```
+Subject             <- rbind(testSubject,trainSubject)
+y                   <- rbind(testy, trainy)
+X                   <- rbind(testX, trainX)
+names(Subject)      <- "subject"
+names(y)            <- "activity"
+names(X)            <- c(as.character(features[, 2]))
+data                <- cbind(Subject, y, X)
+```
+
+Step 2 removes all the non-relevant variables:
+
+```
+Columns             <- grep("-mean\\(\\)|-std\\(\\)", names(data))
+data                <- data[ ,c(1, 2, Columns)]
+```
+
+Step 3 sets meaningful activity labels:
+
+```
+activitylabels[,2]  <- gsub("_",".",tolower(activitylabels[,2]))
+data[,2]            <- activitylabels[data[,2], 2]
+```
+
+Step 4 sets descriptive and appropriate variable labels:
+
+```
+names(data)         <- gsub("-",".",tolower(names(data)))
+names(data)         <- gsub("(","",names(data),fixed=TRUE)
+names(data)         <- gsub(")","",names(data),fixed=TRUE)
+```
+
+Step 5 creates the tidy summary data set:
+
+```
+meandatawide <- ddply(data, c("subject","activity"), numcolwise(mean))
+```
+
+### Usage
+
+1. Create the working directory
+2. Download the UCI HAR data set from [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+3. Unzip the date set and place the "UCI HAR Dataset" folder in the working directory
+4. Check the working directory path is correct in the run_analysis.R script and, if necessary, modify
+5. Check that the R packages plyr & reshape2 are installed, if not then uncomment the relevant lines in the script
+6. Run the run_analysis.R script  
+
+### Input
+
+1. subject_test.txt
+2. y_test.txt
+3. X_test.txt
+4. subject_train.txt
+5. y_train.txt
+6. X_train.txt
+7. features.txt
+8. activity_labels.txt  
+
+For detailed descriptions of these files and their contents see [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) and references therein.  
+ 
+### Output
+
+1. meandatawide.txt  
+
+The output file is placed in the working directory. It is a wide format tidy data set containing the mean values of the selected variables from the input data averaged by subject ID and activity. A detailed description of the ouput variables is given in the Codebook located in the run_analysis.R repo.  
+
+### References
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012. 
---
+++ run_analysis.R
@@ -0,0 +1,103 @@
+## This script reads in the UCI HAR Dataset described in the Getting & Cleaning
+## Data course project and outputs the required tidy data set comprising a 
+## merged set containing average values for the desired mean and std variables
+## per subject and activity with appropriate and descriptive labels.
+
+## Step 0: Set everything up and read in required data etc.
+## --------------------------------------------------------
+
+## Load the needed libraries 
+## install.packages("plyr")
+library(plyr)
+## install.packages("reshape2")
+library(reshape2)
+
+## Set the working directury (this may require modification depending on the 
+## local directory structure used)
+setwd(
+  "~/Documents/Data Science Specialisation/3 - Getting & Cleaning Data/Project")
+
+## Set the paths for the various raw data files etc. This assumes the UCI HAR 
+## data set has been unzipped in the Project directury and not further modified.
+## If it has then, these paths will need modification. This is not loading the 
+## inertial data files since this info would be removed later.
+testSubjectPath     <- "./UCI HAR Dataset/test/subject_test.txt"
+testyPath           <- "./UCI HAR Dataset/test/y_test.txt"
+testXPath           <- "./UCI HAR Dataset/test/X_test.txt"
+trainSubjectPath    <- "./UCI HAR Dataset/train/subject_train.txt"
+trainyPath          <- "./UCI HAR Dataset/train/y_train.txt"
+trainXPath          <- "./UCI HAR Dataset/train/X_train.txt"
+featuresPath        <- "./UCI HAR Dataset/features.txt"
+activityPath        <- "./UCI HAR Dataset/activity_labels.txt"
+
+## Step 1: Merge the training and the test sets to create one data set.
+## --------------------------------------------------------------------
+
+## Load the various raw data files and files containing labels (using read.table
+## as read.csv gets confused)
+testSubject         <- read.table(testSubjectPath)
+testy               <- read.table(testyPath)
+testX               <- read.table(testXPath)
+trainSubject        <- read.table(trainSubjectPath)
+trainy              <- read.table(trainyPath)
+trainX              <- read.table(trainXPath)
+features            <- read.table(featuresPath)
+activitylabels      <- read.table(activityPath)
+
+## Row bind the equivalent test & train data components into a single frame each
+Subject             <- rbind(testSubject,trainSubject)
+y                   <- rbind(testy, trainy)
+X                   <- rbind(testX, trainX)
+
+## Put the column names into the resulting merged data frames
+names(Subject)      <- "subject"
+names(y)            <- "activity"
+names(X)            <- c(as.character(features[, 2]))
+
+## Column bind the three resulting frames to give a single merged frame
+data                <- cbind(Subject, y, X)
+
+## Step 2: Extract only mean and standard deviation for each measurement 
+## ---------------------------------------------------------------------
+
+## Make a vector of columns to keep (1, 2, + col. names containing -mean() or 
+## -std() ) and remove the others from the  data frame (this takes a narrow view 
+## of what the assignment actually means to say here but this can be easily 
+## modified by changing the grep search field in the next line)
+Columns             <- grep("-mean\\(\\)|-std\\(\\)", names(data))
+data                <- data[ ,c(1, 2, Columns)]
+
+## Step 3: Use descriptive activity names for the activities in the data set
+## -------------------------------------------------------------------------
+
+## Modify the activity descriptors to lower case, replace underscores and then 
+## replace the activity codes in the data frame with the modified descriptors
+activitylabels[,2]  <- gsub("_",".",tolower(activitylabels[,2]))
+data[,2]            <- activitylabels[data[,2], 2]
+
+## Step 4: Appropriately label the data set with descriptive variable names 
+## ------------------------------------------------------------------------
+
+## Re-case, replace the hyphens and remove the brackets from variable names
+names(data)         <- gsub("-",".",tolower(names(data)))
+names(data)         <- gsub("(","",names(data),fixed=TRUE)
+names(data)         <- gsub(")","",names(data),fixed=TRUE)
+
+## Step 5: Create a tidy data set with mean(variable) by activity & subject
+## ----------------------------------------------------------------------
+
+## Calculate the new tidy data set with just the mean values of each variable
+## averaged over each activity for each subject in both wide and long formats
+meandatawide <- ddply(data, c("subject","activity"), numcolwise(mean))
+meandatalong <- melt(meandatawide, id.vars=c("subject","activity"))
+meandatalong <- meandatalong[order(meandatalong$subject,meandatalong$activity),]
+
+## Write out the meandata file to the current working drectory (data format
+## is currently wide, if long is needed then write the meandatalong version)
+write.table(meandatawide, file="meandata.txt", row.names=FALSE)
+
+## Clean up memory
+rm(testSubject, testy, testX, trainSubject, trainy, trainX, features, 
+   activitylabels, Subject, y, X, data)
+rm(testSubjectPath, testyPath, testXPath, trainSubjectPath, trainyPath, 
+   trainXPath, featuresPath, activityPath, Columns)
